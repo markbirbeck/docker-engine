eb259927dea13f07c7c057ca91cfc8fd301c7f3a
Mark Birbeck <mark.birbeck@gmail.com>
2018-06-16 17:17:58 +0100

    [#10] Invert server<->ssh relationship

--- src/lib/tunnel.js
+++ src/lib/tunnel.js
@@ -8,58 +8,57 @@ module.exports = async (remoteDocker) => {
     host: remoteDocker,
     privateKey: fs.readFileSync(__dirname + '/docker-swarm.pem', 'utf8')
   }
-  const conn = new Client()
 
   return new Promise((resolve, reject) => {
-    conn.on('error', err => {
-      console.log('SSH - Connection Error: ' + err)
-      reject(err)
-    })
-    conn.on('close', hadError => {
-      console.log('SSH - Connection Closed', hadError)
-      if (hadError) reject()
-    })
-    conn.on('end', () => {
-      console.log('SSH - Connection Ended')
-    })
-    conn.on('authentication', ctx => {
-      console.log('got authentication')
-      ctx.accept()
-    })
-    conn.on('banner', console.log.bind(null, 'got banner:')
-    })
-    conn.on('ready', function() {
-      const f = conn.openssh_forwardOutStreamLocal('/var/run/docker.sock', (err, stream) => {
-        if (err) reject(err)
-        const server = net.createServer(c => {
-          stream.pipe(c).pipe(stream)
-          .on('close', () => {
-            conn.end()
-          })
-          c.on('error', console.error.bind(null, 'proxy request error: '))
-          stream.on('error', console.error.bind(null, 'ssh stream error: '))
-        })
-        // listen on an ephemeral port and modify the input URL to connect to us
-        // instead of the original target
-        server.listen(0, 'localhost', function() {
-          // compose object for connecting to this little TCP proxy server
-          resolve({
-            host: server.address().address,
-            port: server.address().port
-          })
-        })
-
-        server.on('error', console.error.bind(null, 'proxy server error: '))
-
-        /**
-         * Ensure the app can close ok if we're the only code using these
-         * objects:
-         */
-
-        server.unref()
+    const server = net
+    .createServer(sock => {
+      console.log('created server')
+      const conn = new Client()
+      conn.on('ready', () => {
+        console.log('connection ready')
         conn._sock.unref()
+        conn
+        .openssh_forwardOutStreamLocal('/var/run/docker.sock', (err, stream) => {
+          console.log('setting up forwarding')
+          if (err) {
+            console.log('closing connection because forwarding not set:', err)
+            conn.end()
+            return sock.writable && sock.end()
+          }
+
+          if (sock.readable && sock.writable) {
+            console.log('wiring up streams')
+            stream
+            .pipe(sock)
+            .pipe(stream)
+            .on('end', () => {
+              console.log('closing connection after streams have ended')
+              conn.end()
+            })
+            .on('close', () => {
+              console.log('closing connection after streams have closed')
+              conn.end()
+            })
+          } else {
+            console.log('closing connection because stream not read/write')
+            conn.end()
+          }
+        })
+      })
+      .on('error', err => {
+        console.log('error creating client:', err)
+        return sock.writable && sock.end()
+      })
+      .connect(sshOpts)
+    })
+
+    server.listen(0, 'localhost', () => {
+      console.log('server listening')
+      server.unref()
+      resolve({
+        host: server.address().address,
+        port: server.address().port
       })
     })
-    conn.connect(sshOpts)
   })
 }
