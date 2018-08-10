8c269123cd33bba6dd8516b0611088e0a9c4418c
Mark Birbeck <mark.birbeck@gmail.com>
2018-08-10 20:07:50 +0100

    [#15] Add test for container inspection

--- test/DockerEngine.js
+++ test/DockerEngine.js
@@ -23,13 +23,23 @@ tap.test('DockerEngine', async t => {
   t.equal(res.Warnings, null)
   t.ok(res.Id)
 
+  /**
+   * Inspect the container and check a couple of properties:
+   */
+
+  t.comment('inspecting container')
+  const id = res.Id
+  res = await api.ContainerInspect({id})
+  t.same(res.Config.Cmd, body.Cmd)
+  t.same(res.Config.Image, body.Image)
+
   t.comment('deleting the created container')
 
   /**
    * Prepare the request:
    */
 
-  res = await api.ContainerDelete({ id: res.Id })
+  res = await api.ContainerDelete({ id })
 
   /**
    * Nothing is returned, but if there's no error thrown then all is well:
