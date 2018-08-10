a619114c79d15edc37ecbf4475a60e6f4e183ae7
Mark Birbeck <mark.birbeck@gmail.com>
2018-08-10 18:41:28 +0100

    [#15] Add test for deleting container with modem

--- test/SwaggerModem.js
+++ test/SwaggerModem.js
@@ -16,13 +16,13 @@ tap.test('SwaggerModem', async t => {
     Image: 'alpine',
     Cmd: ['/bin/sh', '-c', 'for i in 1 2 3 4 5; do echo "${i}"; sleep 1s; done']
   }
-  const req = builder.ContainerCreate({ body })
+  let req = builder.ContainerCreate({ body })
 
   /**
    * Make the API call:
    */
 
-  const res = await modem.dial(req)
+  let res = await modem.dial(req)
 
   /**
    * There should be no errors, and an ID should be returned:
@@ -30,6 +30,26 @@ tap.test('SwaggerModem', async t => {
 
   t.equal(res.Warnings, null)
   t.ok(res.Id)
+
+  t.comment('deleting the created container')
+
+  /**
+   * Prepare the request:
+   */
+
+  req = builder.ContainerDelete({ id: res.Id })
+
+  /**
+   * Make the API call:
+   */
+
+  res = await modem.dial(req)
+
+  /**
+   * Nothing is returned, but if there's no error thrown then all is well:
+   */
+
+  t.equal(res, '')
 
   t.end()
 })
