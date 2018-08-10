a47a1182acf603183cb7d4e51861ca2f4a8715e2
Mark Birbeck <mark.birbeck@gmail.com>
2018-08-10 17:54:36 +0100

    [#15] Indent request builder test ready to add more

--- test/SwaggerRequestBuilder.js
+++ test/SwaggerRequestBuilder.js
@@ -4,27 +4,28 @@ const Builder = require('../src/lib/SwaggerRequestBuilder')
 const builder = new Builder()
 
 tap.test('SwaggerRequestBuilder', async t => {
-  t.comment('creating request object that will create a container')
-  const body = {
-    Image: 'alpine',
-    Cmd: ['/bin/sh', '-c', 'for i in 1 2 3 4 5; do echo "${i}"; sleep 1s; done']
-  }
-  const req = builder.ContainerCreate({ body })
+  t.test('create a request object for container creation', async t => {
+    const body = {
+      Image: 'alpine',
+      Cmd: ['/bin/sh', '-c', 'for i in 1 2 3 4 5; do echo "${i}"; sleep 1s; done']
+    }
+    const req = builder.ContainerCreate({ body })
 
-  /**
-   * Check that the basic components for a request are present:
-   */
+    /**
+     * Check that the basic components for a request are present:
+     */
 
-  t.equal(req.url, '/v1.37/containers/create')
-  t.equal(req.method, 'POST')
-  t.same(req.body, body)
+    t.equal(req.url, '/v1.37/containers/create')
+    t.equal(req.method, 'POST')
+    t.same(req.body, body)
 
-  /**
-   * Also check that there is a property that tells us how to verify the
-   * result of the request:
-   */
+    /**
+     * Also check that there is a property that tells us how to verify the
+     * result of the request:
+     */
 
-  t.ok(req.responses)
+    t.ok(req.responses)
 
-  t.end()
+    t.end()
+  })
 })
